3b85a2568c015f04d21efaf49a1f8b0d04766360
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-04-06 16:32:07 +0800

    输出ABC*D最大值

---
+++ ABCmultipleD.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int a[10];
+int book[10];
+int n;
+int b[10];
+int maxn = 0;int res = 0;
+void dfs(int k) {
+	if (k >= n) {
+		res = (a[0] * 100 + a[1] * 10 + a[2]) * a[3];//记录分支的值
+		if (maxn<= res)maxn= res;
+		return;
+	}
+	
+	for (int i = 0; i < n; i++) {
+		if (book[i] == 1)continue;
+		a[k] = b[i];
+		book[i] = 1;//表示该数字已经被使用过
+		dfs(k + 1);
+		book[i] = 0;//此分支已经已经退出，重置book[i]的状态
+	}
+
+}
+int main() {
+	n = 4;
+	while (cin >> b[0] >> b[1] >> b[2] >> b[3]) {
+		maxn = 0;
+		dfs(0);
+		cout << maxn << endl;
+	}
+
+	return 0;
+}
