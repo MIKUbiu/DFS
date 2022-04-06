19f30fa4007d5c5212e12db5646f3614074e2bcc
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-04-06 16:05:58 +0800

    全排列

---
+++ fullPermutation.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int a[10];
+int book[10];
+int n;
+void dfs(int k) {
+	if (k >= n) {
+	for (int i = 0; i < n; i++) {
+		cout << a[i];
+		}
+	cout << endl;
+	return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (book[i] == 1)continue;//如果父节点使用过该数，就不继续这个分支
+		a[k] = i+1;
+		book[i] = 1;//表示该数字已经被使用过
+		dfs(k + 1);
+		book[i] = 0;//此分支已经已经退出，重置book[i]的状态
+	}
+
+}
+int main() {
+	cin >> n;
+	dfs(0);
+	return 0;
+}
